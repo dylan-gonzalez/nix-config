32e9d2f7cb4da98c23979f3b7e54c72f8d07a24f
Dylan Gonzalez <dylcg10@gmail.com>
2024-01-02 22:30:24 +1100

    feature: mullvad-vpn, transmission

--- homelab/default.nix
+++ homelab/default.nix
@@ -13,6 +13,29 @@ in
       jellyfin-ffmpeg
     ];
 
+    programs.bash.shellAliases = {
+      tsm = "transmission-remote";
+    };
+
+    services = {
+      jellyfin = {
+        user = "dylan";
+      };
+
+      mullvad-vpn.enable = true;
+      transmission = {
+        enable = true; #Enable transmission daemon
+        openRPCPort = true; #Open firewall for RPC
+        settings = { #Override default settings
+          download-dir = "/mnt/hdd/home/dylan/jellyfin_library/movies/";
+          incomplete-dir = "/home/dylan/Downloads";
+          rpc-bind-address = "0.0.0.0"; #Bind to own IP
+          rpc-whitelist = "127.0.0.1,10.0.0.1"; #Whitelist your remote machine (10.0.0.1 in this example)
+          script-torrent-done-enabled = true;
+        };
+      };
+    };
+
     systemd.targets = {
       sleep.enable = false;
       suspend.enable = false;
