065aa3448f36b6fa14a8f93f12bd950037565c62
Dylan Gonzalez <dylcg10@gmail.com>
2024-06-04 14:45:16 +1000

    separate nixos and home manager config

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,7 +1,48 @@
+{ pkgs, config, lib, ... }:
+
 {
 	imports = [
 		./desktop
 		./user
 		./networking
 	];
+
+  config = {
+    home-manager.dylan.sharedModules = [
+      ../home
+    ];
+    
+    hardware.pulseaudio.enable = false;
+
+    home-manager.useUserPackages = true;
+
+    security.rtkit.enable = true;
+    services = {
+      localtimed.enable = true;
+
+      jellyfin.enable = true;
+
+      openssh.enable = true;
+
+      pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+      };
+
+      tailscale.enable = true;
+
+      xserver = {
+        enable = true;
+        displayManager.gdm.enable = true;
+        displayManager.gdm.wayland = false;
+        desktopManager.gnome.enable = true;
+
+        excludePackages = with pkgs; [
+          xterm
+        ];
+      };
+    };
+  };
 }
