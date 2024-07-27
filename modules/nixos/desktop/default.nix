b76724b2d62974fb4b7e377158fcd2414ddc397e
Dylan Gonzalez <dylcg10@gmail.com>
2024-07-27 21:32:47 +1000

    gnome packages moved to top-level

--- modules/nixos/desktop/default.nix
+++ modules/nixos/desktop/default.nix
@@ -11,15 +11,15 @@ in
       gnome-photos
       gnome-tour
       gedit
-    ]) ++ (with pkgs.gnome; [
-      cheese
-      gnome-music
-      gnome-terminal
-      epiphany
       geary
       evince
-      gnome-characters
       totem
+      epiphany
+      gnome-terminal
+      cheese
+    ]) ++ (with pkgs.gnome; [
+      gnome-music
+      gnome-characters
       tali
       iagno
       hitori
